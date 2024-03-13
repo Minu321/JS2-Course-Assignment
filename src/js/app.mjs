b2325691b840015e59052b4abc4ca1c10b90b2cc
Mina <mina_rb@outlook.com>
2024-03-13 13:19:58 +0100

    added authorization
    can now register and log in

--- src/js/app.mjs
+++ src/js/app.mjs
@@ -1,4 +1,17 @@
+import { setRegisterFormListener } from "./handlers/register.mjs";
+import { setLoginFormListener } from "./handlers/login.mjs";
+
+setRegisterFormListener();
+setLoginFormListener();
+
+const path = location.pathname;
+if (path == "/profile/login/") {
+  setLoginFormListener();
+} else if (path == "/profile/register/") {
+  setRegisterFormListener();
+}
+
 import * as constants from "./api/constants.mjs";
 
-console.log(constants.API_URL);
+console.log(constants.API_SOCIAL_URL);
 console.log(8);
---
+++ src/js/handlers/login.mjs
@@ -0,0 +1,17 @@
+import { login } from "../api/auth/login.mjs";
+
+export function setLoginFormListener() {
+  const form = document.querySelector("#loginUser");
+  if (form) {
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const form = event.target;
+      const formData = new FormData(form);
+      const profile = Object.fromEntries(formData.entries());
+
+      console.log(profile);
+
+      login(profile);
+    });
+  }
+}
